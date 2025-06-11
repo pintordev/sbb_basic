01b410f557846d2bbda267ad1949d5c80c0955eb
pintor.dev <pintor.dev@gmail.com>
2025-06-11 23:46:47 +0900

    chore: env setting

---
+++ src/main/java/com/pintor/sbb_basic/SbbBasicApplication.java
@@ -0,0 +1,13 @@
+package com.pintor.sbb_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pintor/sbb_basic/SbbBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pintor.sbb_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
